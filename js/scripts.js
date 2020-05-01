8303a481b518d19766d569f5b2690411008d88a2
Isaac Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2020-05-01 11:42:47 -0700

    add additional functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 var add = function(number1, number2) {
     return number1 + number2;
   };
+
+var tempConvert = function(answer) {
+    return (answer * -17.222);
+};
   
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
   var result = add(number1, number2);
   alert(result);
+  var result = parseInt(prompt("Enter a temperature"));
+  var answer = tempConvert(result);
+  alert(answer);
   
